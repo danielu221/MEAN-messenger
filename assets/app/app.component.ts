02cf43b3c5651a1512325675207c68fc323664aa
Mateusz Danieluk <danielukmateusz@gmail.com>
2017-09-08 15:27:29 +0200

    Added messages component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { Message } from "./messages/message.model"
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls:['app.component.css']
 })
 export class AppComponent {
-    
+    messages:Message[]=
+    [
+      new Message ("Content of message1", "Matt"),
+      new Message ("Content of message2", "Matt")
+    ];
 }
