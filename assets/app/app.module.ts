58e1b3ade5900d5d80e2c261613a46c3cdc6b42f
Mateusz Danieluk <danielukmateusz@gmail.com>
2017-09-12 15:53:28 +0200

    Add header component

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
-import{ MessageComponent } from "./messages/message.component"
-import{ MessageListComponent } from "./messages/message-list.component"
-import{ MessageInputComponent } from "./messages/message-input.component"
-import{ MessagesComponent } from "./messages/messages.component"
+import { MessageComponent } from "./messages/message.component"
+import { MessageListComponent } from "./messages/message-list.component"
+import { MessageInputComponent } from "./messages/message-input.component"
+import { MessagesComponent } from "./messages/messages.component"
+import { AuthenticationComponent } from "./auth/authentication.component"
+import { HeaderComponent } from "./header.component"
 
 @NgModule({
     declarations: [
@@ -14,7 +16,9 @@ import{ MessagesComponent } from "./messages/messages.component"
         MessageComponent,
         MessageListComponent,
         MessageInputComponent,
-        MessagesComponent
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
     imports: [BrowserModule,FormsModule],
     bootstrap: [AppComponent]
