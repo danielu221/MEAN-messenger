2516c1c2141c86b6923cb39d60eaeddfbd6fa4f9
Mateusz Danieluk <danielukmateusz@gmail.com>
2017-09-15 14:28:48 +0200

    Add baisc auth forms

--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -4,12 +4,17 @@ import { Component} from '@angular/core';
 @Component({
     selector: 'app-authentication',
     template: `
-    <div class="row">
-      <app-message-input></app-message-input>
-    </div>
-    <hr>
-    <div class="row">
-      <app-message-list></app-message-list>
+    <header class="row spacing">
+      <nav class="col-md-8 col-md-offset-2">
+          <ul class="nav nav-tabs">
+              <li><a [routerLink]="['signup']">Signup</a></li>
+              <li><a [routerLink]="['signin']">Signin</a></li>
+              <li><a [routerLink]="['logout']">Logout</a></li>
+          </ul>
+      </nav>
+    </header>
+    <div class="row spacing">
+      <router-outlet></router-outlet>
     </div>`
 })
 
