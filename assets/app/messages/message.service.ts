0a7770aca8caa33dbb77d88cc3dbcf606174a1fc
Mateusz Danieluk <danielukmateusz@gmail.com>
2017-10-16 14:41:23 +0200

    Add author to proper message

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -20,7 +20,7 @@ export class MessageService{
     return this.http.post('http://localhost:3000/message' + token,body,{headers:headers})
       .map((response: Response) => {
         const result = response.json();
-        const message = new Message(result.obj.content,"dummy",result.obj._id,null);
+        const message = new Message(result.obj.content,result.obj.user.firstname,result.obj._id,result.obj.user._id);
         this.messages.push(message);
         return message;
       })
@@ -33,7 +33,7 @@ export class MessageService{
         const messages=response.json().obj;
         let transformedMessages: Message[]=[];
         for (let message of messages){
-          transformedMessages.push(new Message(message.content, 'Dummy', message._id,null))
+          transformedMessages.push(new Message(message.content, message.user.firstName, message._id, message.user._id))
         }
         this.messages=transformedMessages;
         return transformedMessages;
--- routes/user.js
+++ routes/user.js
@@ -54,7 +54,8 @@ router.post('/signin',function(req, res, next){
     res.status(200).json({
       message:'Successfully logged in',
       token:token,
-      userId:user._id
+      userId:user._id,
+      userMail:user.email
     })
 
   })
